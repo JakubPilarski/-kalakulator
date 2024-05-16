37ed6df73e9c5a03bdb7b5f58db54f83abada147
JakubPilarski <250991@edu.p.lodz.pl>
2024-05-16 20:37:25 +0200

    dzielenie

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@ void menu(void) {
     printf("1 - Add\n");
     printf("2 - Subtract\n");
     printf("3 - Multiply\n");
-    printf("4 - Stop Program\n");
+    printf("4 - Divide\n");
+    printf("5 - Stop Program\n");
     printf("Select an option:\n");
 }
 int main() {
@@ -13,7 +14,9 @@ int main() {
     int number1 = 0;
     int number2 = 0;
     int result = 0;
-
+    float number3 = 0;
+    float number4 = 0;
+    float resultd = 0;
     do {
         menu();
         scanf("%d", &option);
@@ -44,10 +47,27 @@ int main() {
                 result = number1 * number2;
                 printf("%d * %d = %d\n", number1, number2, result);
                 break;
+                case 4:
+                printf("First number:");
+                scanf("%f", &number3);
+                printf("Second number:");
+                scanf("%f", &number4);
+                if (number4!= 0)
+                {
+                    resultd = number3 / number4;
+                    printf("%f / %f = %f\n", number3, number4, resultd);
+                }
+
+                   else
+                    {
+                        printf("%f / %f = wrong\n try again\n", number3, number4);
+                    }
+
+                break;
             default:
                 printf("Choose the right option\n\n");
         }
-    } while (option != 4);
+    } while (option != 5);
 
     return 0;
 }
