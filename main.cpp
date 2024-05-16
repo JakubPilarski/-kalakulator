dfb05757e7cce6abe715b6b2d1b8551279abe9e7
JakubPilarski <250991@edu.p.lodz.pl>
2024-05-16 20:22:01 +0200

    mnozenie

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@ void menu(void) {
     printf("\n");
     printf("1 - Add\n");
     printf("2 - Subtract\n");
-    printf("3 - Stop Program\n");
+    printf("3 - Multiply\n");
+    printf("4 - Stop Program\n");
     printf("Select an option:\n");
 }
 int main() {
@@ -35,10 +36,18 @@ int main() {
                 result = number1 - number2;
                 printf("%d - %d = %d\n", number1, number2, result);
                 break;
+            case 3:
+                printf("First number:");
+                scanf("%d", &number1);
+                printf("Second number:");
+                scanf("%d", &number2);
+                result = number1 * number2;
+                printf("%d * %d = %d\n", number1, number2, result);
+                break;
             default:
                 printf("Choose the right option\n\n");
         }
-    } while (option != 3);
+    } while (option != 4);
 
     return 0;
 }
